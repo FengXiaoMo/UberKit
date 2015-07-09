0f8d321740306c3d7e0399be068f26339ce75821
Boris <zhouboli@gmail.com>
2015-07-09 10:14:02 +0800

    Create an API to set access token so no need to request for new one each time within 30 days

--- UberKit/UberKit.m
+++ UberKit/UberKit.m
@@ -93,6 +93,11 @@ NSString * const mobile_safari_string = @"com.apple.mobilesafari";
     return _accessToken;
 }
 
+- (void) setAuthTokenWith:(NSString *)token
+{
+    _accessToken = token;
+}
+
 #pragma mark - Product Types
 
 - (void) getProductsForLocation:(CLLocation *)location withCompletionHandler:(CompletionHandler)completion
